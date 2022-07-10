11648f2fa0f082b3804366dd94cd31c6e98fd385
Gabriel Lopez <gla280900@gmail.com>
2022-07-10 16:27:20 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+from src import create_app
+
+app = create_app()
+
+def page_not_found(error):
+    return "<h1>Not found page</h1>", 404
+
+if __name__ == '__main__':
+    # Error handlers
+    app.register_error_handler(404, page_not_found)
+    app.run(debug=True)
---
+++ src/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_login.utils import login_required
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager, login_manager
+from decouple import config
+
+# Routes
+# from ..routes.views import views
+
+
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
+
+app = Flask(__name__)
+
+@app.route('/')
+def create_app():
+    
+    # app.config['SECRET_KEY'] = config('SECRET_KEY')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # db.init_app(app)    
+
+    # Blueprints
+    # app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(views, url_prefix='/')
+
+    # from ..models.models import User, Note
+    # # create_database(app) 
+
+    # login_manager = LoginManager()
+    # login_manager.login_view = 'auth.login'
+    # login_manager.init_app(app)
+    
+    # @login_manager.user_loader
+    # def load_user(id):
+    #     return User.query.get(int(id))
+
+    return 'Hello World!'
+
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
