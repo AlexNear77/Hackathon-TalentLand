90e0412c92ef6d66d2614b2a75e356833a56a228
Gabriel Lopez <gla280900@gmail.com>
2022-07-10 17:02:22 -0500

    test

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
-from src import create_app
+# from src import create_app
 
-app = create_app()
+# # app = create_app()
 
-def page_not_found(error):
-    return "<h1>Not found page</h1>", 404
+# def page_not_found(error):
+#     return "<h1>Not found page</h1>", 404
 
-if __name__ == '__main__':
-    # Error handlers
-    app.register_error_handler(404, page_not_found)
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     # Error handlers
+#     app.register_error_handler(404, page_not_found)
+#     app.run()
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
