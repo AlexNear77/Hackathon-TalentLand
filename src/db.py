c7b5cd49431413d306dc26fbcdc985d998afbe52
Gabriel Lopez <gla280900@gmail.com>
2022-07-17 18:00:53 -0500

    change DB to mmsql

--- src/db.py
+++ src/db.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from decouple import config
 
+import urllib
 
-database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+# params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 10.0};SERVER=dagger;DATABASE=test;UID=user;PWD=password")
+# engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+
+# Connect String: mssql+pyodbc://{dbuser}:{dbpass}@{dbhost}/{dbname}
+database_uri = 'mssql+pyodbc://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser = config('DBUSER'),
     dbpass = config('DBPASS'),
     dbhost = config('DBHOST'),
     dbname = config('DBNAME')
 )
 
-# def create_app():
+# app = Flask(__name__)
+# engine = create_engine(database_uri)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# # # def create_app():
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 
@@ -24,4 +36,5 @@ app.config.update(
 db = SQLAlchemy(app)
 
 # initialize database migration management
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
+
